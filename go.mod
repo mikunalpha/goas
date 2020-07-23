269f0998cedf5a8493ff595367fb29d372cd82b5
ImVexed <icemantheditor@gmail.com>
2020-07-23 12:00:04 -0700

    remove cli dep

--- go.mod
+++ go.mod
@@ -1,10 +1,9 @@
-module github.com/mikunalpha/goas
+module github.com/ImVexed/goas
 
 go 1.12
 
 require (
 	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0
 	github.com/mikunalpha/go-module v0.0.0-20190521120234-12aa2dc244ca
-	github.com/urfave/cli v1.20.0
-	github.com/uudashr/go-module v0.0.0-20180827225833-c0ca9c3a4966 // indirect
+	github.com/uudashr/go-module v0.0.0-20200529023307-c90a4239ad70 // indirect
 )
--- main.go
+++ main.go
@@ -1,67 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"os"
-
-	"github.com/urfave/cli"
 )
 
-var version = "v1.0.0"
-
-var flags = []cli.Flag{
-	cli.StringFlag{
-		Name:  "module-path",
-		Value: "",
-		Usage: "goas will search @comment under the module",
-	},
-	cli.StringFlag{
-		Name:  "main-file-path",
-		Value: "",
-		Usage: "goas will start to search @comment from this main file",
-	},
-	cli.StringFlag{
-		Name:  "handler-path",
-		Value: "",
-		Usage: "goas only search handleFunc comments under the path",
-	},
-	cli.StringFlag{
-		Name:  "output",
-		Value: "oas.json",
-		Usage: "output file",
-	},
-	cli.BoolFlag{
-		Name:  "debug",
-		Usage: "show debug message",
-	},
-}
-
-func action(c *cli.Context) error {
-	p, err := newParser(c.GlobalString("module-path"), c.GlobalString("main-file-path"), c.GlobalString("handler-path"), c.GlobalBool("debug"))
-	if err != nil {
-		return err
-	}
-	// fmt.Printf("%+v\n", p)
-	return p.CreateOASFile(c.GlobalString("output"))
-}
-
 func main() {
-	app := cli.NewApp()
-	app.Name = "goas"
-	app.Usage = ""
-	// app.UsageText = "goas [options]"
-	app.Version = version
-	app.Copyright = "(c) 2018 mikun800527@gmail.com"
-	app.HideHelp = true
-	app.OnUsageError = func(c *cli.Context, err error, isSubcommand bool) error {
-		cli.ShowAppHelp(c)
-		return nil
-	}
-	app.Flags = flags
-	app.Action = action
+	modulePath := flag.String("module-path", "", "goas will search @comment under the module")
+	mainFilePath := flag.String("module-file-path", "", "goas will start to search @comment from this main file")
+	handlerPath := flag.String("handler-path", "", "goas only search handleFunc comments under the path")
+	output := flag.String("output", "oas.json", "output file")
+	debug := flag.Bool("debug", false, "show debug message")
 
-	err := app.Run(os.Args)
+	p, err := newParser(*modulePath, *mainFilePath, *handlerPath, *debug)
 	if err != nil {
-		log.Fatal("Error: ", err)
+		log.Fatal(err)
+	}
+
+	if err := p.CreateOASFile(*output); err != nil {
+		log.Fatal(err)
 	}
 }
