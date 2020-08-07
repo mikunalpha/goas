4c93c7817e0538121dfb74ebdaa82f07ef228f94
Nico Cesar <nico@nicocesar.com>
2020-08-07 11:29:42 -0400

    Revert "remove cli dep"
    This reverts commit 269f0998cedf5a8493ff595367fb29d372cd82b5.

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0
+	github.com/urfave/cli v1.20.0
 	golang.org/x/mod v0.3.0
 )
--- main.go
+++ main.go
@@ -1,23 +1,67 @@
 package main
 
 import (
-	"flag"
 	"log"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
-func main() {
-	modulePath := flag.String("module-path", "", "goas will search @comment under the module")
-	mainFilePath := flag.String("module-file-path", "", "goas will start to search @comment from this main file")
-	handlerPath := flag.String("handler-path", "", "goas only search handleFunc comments under the path")
-	output := flag.String("output", "oas.json", "output file")
-	debug := flag.Bool("debug", false, "show debug message")
+var version = "v1.0.0"
 
-	p, err := newParser(*modulePath, *mainFilePath, *handlerPath, *debug)
+var flags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "module-path",
+		Value: "",
+		Usage: "goas will search @comment under the module",
+	},
+	cli.StringFlag{
+		Name:  "main-file-path",
+		Value: "",
+		Usage: "goas will start to search @comment from this main file",
+	},
+	cli.StringFlag{
+		Name:  "handler-path",
+		Value: "",
+		Usage: "goas only search handleFunc comments under the path",
+	},
+	cli.StringFlag{
+		Name:  "output",
+		Value: "oas.json",
+		Usage: "output file",
+	},
+	cli.BoolFlag{
+		Name:  "debug",
+		Usage: "show debug message",
+	},
+}
+
+func action(c *cli.Context) error {
+	p, err := newParser(c.GlobalString("module-path"), c.GlobalString("main-file-path"), c.GlobalString("handler-path"), c.GlobalBool("debug"))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+	// fmt.Printf("%+v\n", p)
+	return p.CreateOASFile(c.GlobalString("output"))
+}
 
-	if err := p.CreateOASFile(*output); err != nil {
-		log.Fatal(err)
+func main() {
+	app := cli.NewApp()
+	app.Name = "goas"
+	app.Usage = ""
+	// app.UsageText = "goas [options]"
+	app.Version = version
+	app.Copyright = "(c) 2018 mikun800527@gmail.com"
+	app.HideHelp = true
+	app.OnUsageError = func(c *cli.Context, err error, isSubcommand bool) error {
+		cli.ShowAppHelp(c)
+		return nil
+	}
+	app.Flags = flags
+	app.Action = action
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal("Error: ", err)
 	}
 }
