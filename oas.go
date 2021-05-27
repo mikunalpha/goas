e6e1c730475e7b5fee92128ccddd969cc58f4fa9
Thorsten Riess <tr@scientific-wave.com>
2021-05-27 15:41:22 +0200

    Fix indent

--- oas.go
+++ oas.go
@@ -144,7 +144,7 @@ type SchemaObject struct {
 	Items       *SchemaObject          `json:"items,omitempty"` // use ptr to prevent recursive error
 	Example     interface{}            `json:"example,omitempty"`
 	Deprecated  bool                   `json:"deprecated,omitempty"`
-	Nullable bool `json:"nullable,omitempty"`
+	Nullable    bool                   `json:"nullable,omitempty"`
 
 	// Ref is used when SchemaObject is as a ReferenceObject
 	Ref string `json:"$ref,omitempty"`
